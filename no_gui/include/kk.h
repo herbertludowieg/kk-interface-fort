e9c833e18bfd6a4293656b6d9e709b41c139ef3c
Herbert Ludowieg <herbertl@buffalo.edu>
2017-05-22 04:11:37 -0400

    Adding initial files and directories to build on

---
+++ no_gui/include/kk.h
@@ -0,0 +1,16 @@
+#ifndef KK_H
+#define KK_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+class KK {
+  public:
+    KK ()
+    ~KK ()
+    void input ( std::ostream & out = std::cout )
+    
+};
+#endif
---
+++ no_gui/src/kk.cc
@@ -0,0 +1,8 @@
+#include "include/kk.h"
+
+KK::KK() {}
+KK::~KK() {}
+
+bool KK::input ( std::ostream & out ) {
+  
+}
